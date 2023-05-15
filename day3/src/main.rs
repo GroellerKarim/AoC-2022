111601c23bfb1d7ec09bc8d1e7c378351ef06010
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-05-15 14:53:18 +0200

    First Star of Day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::File,
-    io::{self, BufRead, BufReader, Lines},
+    io::{self, BufRead},
     path::Path,
 };
 
@@ -21,20 +21,31 @@ fn main() {
 }
 
 fn calculate_line(line: String) -> u32 {
+
+    let mid = line.chars().count() / 2;
+    let _first_compartment = &line[..mid];
+    let _second_compartment= &line[mid..];
+
+    println!("line {}", line);
+    println!("first comp {}, second comp {}", _first_compartment, _second_compartment);
+
     let mut last_char: char = '|';
-    for char in line.chars() {
-        if char == last_char { break };
+    
+    'outer: for char in _first_compartment.chars() {
         last_char = char;
+        for char2 in _second_compartment.chars() {
+            if char2 == last_char { break 'outer; }
+        }
     }
-    println!("{}", last_char);
-    let num: u32 = 31;
-    let mut priority: u32 = last_char.to_digit(2).unwrap() & num;
+
+    let num: u32 = 31; 
+    let mut priority: u32 = (last_char as u32) & num;
 
     if last_char.is_uppercase() { priority += 26 }
 
+    println!("priority for char {} is {}", last_char, priority);
     return priority;
 }
-
 
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
